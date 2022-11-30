568289bcef388ebfb590bde4e45511333dd290a8
Maxime Désécot <maxime@zextras.fr>
2022-11-30 19:01:20 +0100

    refactor(Struct::Base): replace define_method by class_eval

--- lib/struct/base.rb
+++ lib/struct/base.rb
@@ -4,11 +4,17 @@ module Raoh
   module Struct
     class Base
       class << self
+        def attributes
+          @attributes ||= []
+        end
+
         def attribute(attr_name, attr_type, options = {})
           define_attribute_method(AttributeOption.new(attr_name, attr_type, options))
         end
 
         def define_attribute_method(recorded_attribute)
+          attributes << recorded_attribute
+
           define_attribute_getter_method(recorded_attribute)
           define_attribute_has_changed_method(recorded_attribute)
           define_attribute_has_was_method(recorded_attribute)
@@ -16,43 +22,50 @@ module Raoh
         end
 
         def define_attribute_getter_method(recorded_attribute)
-          define_method(recorded_attribute.attr_name) do |*_args|
-            private_instance_variable(recorded_attribute).get
-          end
+          class_eval <<-CODE, __FILE__, __LINE__ + 1
+            def #{recorded_attribute.attr_name}
+              @_#{recorded_attribute.attr_name}.get
+            end
+          CODE
         end
 
         def define_attribute_has_changed_method(recorded_attribute)
-          method_name = "#{recorded_attribute.attr_name}_changed?"
-          define_method(method_name) do |*_args|
-            private_instance_variable(recorded_attribute).changed?
-          end
+          class_eval <<-CODE, __FILE__, __LINE__ + 1
+            def #{recorded_attribute.attr_name}_changed?
+              @_#{recorded_attribute.attr_name}.changed?
+            end
+          CODE
         end
 
         def define_attribute_has_was_method(recorded_attribute)
-          method_name = "#{recorded_attribute.attr_name}_was"
-          define_method(method_name) do |*_args|
-            private_instance_variable(recorded_attribute).was
-          end
+          class_eval <<-CODE, __FILE__, __LINE__ + 1
+            def #{recorded_attribute.attr_name}_was
+              @_#{recorded_attribute.attr_name}.was
+            end
+          CODE
         end
 
         def define_attribute_setter_method(recorded_attribute)
-          method_name = "#{recorded_attribute.attr_name}="
-          define_method(method_name) do |value|
-            private_instance_variable(recorded_attribute).set(value)
-          end
+          class_eval <<-CODE, __FILE__, __LINE__ + 1
+            def #{recorded_attribute.attr_name}=(value)
+              @_#{recorded_attribute.attr_name}.set(value)
+            end
+          CODE
         end
       end
 
       def initialize
+        define_attribute_variables
         yield(self) if block_given?
       end
 
-      def private_instance_variable(recorded_attribute)
-        attr = instance_variable_get(recorded_attribute.instance_variable_key)
-        return attr unless attr.nil?
+      private
 
-        attr = Raoh::Attribute.new(recorded_attribute.attr_type, recorded_attribute.options)
-        instance_variable_set(recorded_attribute.instance_variable_key, attr)
+      def define_attribute_variables
+        self.class.attributes.each do |recorded_attribute|
+          attr = Raoh::Attribute.new(recorded_attribute.attr_type, recorded_attribute.options)
+          instance_variable_set(recorded_attribute.instance_variable_key, attr)
+        end
       end
     end
   end
